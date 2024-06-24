abc6084e12ff3718e8b69256b2d56ca3fd1c98a4
kcaakash49 <kcaakash4910@gmail.com>
2024-06-24 20:17:25 +0545

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req,res) => {
+    res.send("Hello")
+})
+
+app.listen(port,()=>{
+    console.log(`Server started at port ${port}`)
+})
+
