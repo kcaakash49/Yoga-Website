a986923e05003bc910b7570b41c00ce2af5b29d6
kcaakash49 <kcaakash4910@gmail.com>
2024-06-26 20:45:08 +0545

    init

--- backend/db.js
+++ backend/db.js
@@ -22,21 +22,35 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Classes'
   }],
-  cartItems : [{
+  cartItems: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref:"Cart"
-  }]
+    ref: "Cart"
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const classSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number
-})
-
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  availableSeats: { type: Number, required: true },
+  videoLink: { type: String, required: true },
+  instructorName: { type: String, required: true },
+  instructorEmail: { type: String, required: true },
+  totalEnrolled: { type: Number, required: true },
+  reason: { type: String, required: false },
+  status:{type:String, required:true},
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 const cartSchema = new mongoose.Schema({
-    title: String,
-    price: Number
+  title: String,
+  price: Number
 })
 
 
