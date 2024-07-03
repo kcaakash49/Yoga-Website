9c1a76066dd2e545212048c609c987e3280acc2f
kcaakash49 <kcaakash4910@gmail.com>
2024-07-03 11:14:09 +0545

    init

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const navItems = [
+    { name: "Home" , route: "/"},
+    { name: "Instructors" , route: "/instructors"},
+    { name: "Classes" , route: "/class"},
+]
+const Navbar = () => {
+  return (
+    <nav>Navbar</nav>
+  )
+}
+
+export default Navbar
--- frontend/src/router/MainRouter.jsx
+++ frontend/src/router/MainRouter.jsx
@@ -5,11 +5,15 @@ import {
 
 } from "react-router-dom";
 import PageNotFound from "../pages/PageNotFound";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
 
 export const MainRouter = createBrowserRouter(
     createRoutesFromElements(
         <Route>
-            
+            <Route path = "/" element = {<MainLayout/>}>
+                <Route index element = {<Home/>}/>
+            </Route>
             <Route path="*" element={<PageNotFound />} />
 
         </Route>
